98450e6f9b0edff348f494d8f82af2dd04f0c32a
athittaya85 <89177812+athittaya85@users.noreply.github.com>
2021-11-26 21:35:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -154,16 +154,16 @@ def event_handle(event,json_line):
             line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "กินข้าวไหม":
              replyObj = TextSendMessage(text="ไม่ล่ะ ขอเป็นสเต็ก")
-             line_bot_api.reply_message(rtoken, replyObj)
+            line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "ไปเที่ยวกันไหม":
             replyObj = TextSendMessage(text="ไปดิ")
             line_bot_api.reply_message(rtoken, replyObj)
         else : 
-            headers = request.headers
-            json_headers = ({k:v for k, v in headers.items()})
-            json_headers.update({'Host':'bots.dialogflow.com'})
-            url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/efdb8f5c-1118-491d-ad3a-1d73d4b4f444"
-            requests.post(url,data=json_line, headers=json_headers)
+           headers = request.headers
+           json_headers = ({k:v for k, v in headers.items()})
+           json_headers.update({'Host':'bots.dialogflow.com'})
+           url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/efdb8f5c-1118-491d-ad3a-1d73d4b4f444"
+           requests.post(url,data=json_line, headers=json_headers)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
